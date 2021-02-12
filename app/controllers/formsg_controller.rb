f952fd77574ec187e29de871e52190bb0b20b3e5
Michael Cheng <michael_cheng@tech.gov.sg>
2021-02-12 17:38:40 +0800

    Update to use the Service class pattern

--- app/controllers/formsg_controller.rb
+++ app/controllers/formsg_controller.rb
@@ -14,11 +14,11 @@ class FormsgController < ApplicationController
 
     # Step 3: Decrypt the form submission
     # Get just the responses as a Hash
-    result_hash = Formsg::Sdk::Crypto.new.decrypt(data: payload)
+    result_hash = Formsg::Sdk::SubmissionService.decrypt_for(data: payload)
     Rails.logger.info "Submission Result (Hash): #{result_hash.inspect}"
 
     # Get the Submission & Responses as an object
-    submission = Formsg::Sdk::Models::Submission.build_from(data: payload)
+    submission = Formsg::Sdk::SubmissionService.build_from(data: payload)
     Rails.logger.info "Submission Result (Object): #{submission.inspect}"
 
     head :ok
